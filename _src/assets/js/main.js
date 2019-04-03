324089fa427ac6659119f5f45fcf41dbf28deb45
leireriel <leirericop@gmail.com>
2019-04-03 13:01:09 +0200

    finish event for dialogue, click type

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,2 +1,10 @@
 'use strict';
 
+const dialogue = document.querySelector('.first-dialogue_wrapper');
+
+const hide = function(event) {
+    dialogue.classList.add('hide-first-dialogue_wrapper');
+}
+
+dialogue.addEventListener('click', hide);
+
