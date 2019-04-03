7d832ae004003e40a088d8413d47f86f5d8ff264
leireriel <leirericop@gmail.com>
2019-04-03 13:23:50 +0200

    finish event with setTimeout typegit add -A!

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,9 +2,14 @@
 
 const dialogue = document.querySelector('.first-dialogue_wrapper');
 
-const hide = function(event) {
-    dialogue.classList.add('hide-first-dialogue_wrapper');
-}
+// const hide = function(event) {
+//     dialogue.classList.add('hide-first-dialogue_wrapper');
+// }
 
-dialogue.addEventListener('mouseover', hide);
+// dialogue.addEventListener('mouseover', hide);
+
+setTimeout(function() {
+    dialogue.classList.add('hide-first-dialogue_wrapper');
+}, 3000
+);
 
