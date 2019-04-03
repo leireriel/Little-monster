eec00c415673130e32217ebe582c2afbc52815c5
leireriel <leirericop@gmail.com>
2019-04-03 13:27:11 +0200

    finish proyectgit add -A :D

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,14 +2,14 @@
 
 const dialogue = document.querySelector('.first-dialogue_wrapper');
 
+setTimeout(function() {
+    dialogue.classList.add('hide-first-dialogue_wrapper');
+}, 3000
+);
+
 // const hide = function(event) {
 //     dialogue.classList.add('hide-first-dialogue_wrapper');
 // }
 
 // dialogue.addEventListener('mouseover', hide);
-
-setTimeout(function() {
-    dialogue.classList.add('hide-first-dialogue_wrapper');
-}, 3000
-);
 
